0249d9fe4981e22f71825af0c370dbb180ffa461
Chandra Galbiati <chandra.galbiati@gmail.com>
2016-04-25 03:45:04 -0400

    fixed negative indexing error

---
+++ bytely/createurlhash.py
@@ -0,0 +1,61 @@
+import hashlib
+from .models import Url
+
+
+
+# returns unique string for the short link or existing short link if url is already in db
+def create_entry(source_url):
+    # shaw hash
+    hashed_url = hashlib.sha224(source_url).hexdigest()
+
+    # add http so redirect works
+    if not 'http' in source_url:
+        source_url = 'http://' + source_url
+    # it's up to the user to put in a valid link
+    print('creating for', source_url)
+
+    #max length of hash to try
+    max_length = 4
+    avail_hash = ''
+    
+    while not avail_hash:
+        # build string list of slices to use in query 
+        hash_slices = [hashed_url[0]]
+        for i in range(2, max_length):
+            hash_slices.append(hashed_url[0:i])
+
+        print('hash slices', hash_slices)
+        # query db for slices
+        found = Url.objects.filter(short_url__in=hash_slices).extra(select={'length':'Length(short_url)'}).order_by('-length')
+        print('found', found)
+
+        # find shortest avail
+
+        # look for url match
+        for item in found: 
+            if item.source_url == source_url:
+                return item
+
+        # see if there is an available hash with length less than or equal to max_length
+        if len(found) == 0:
+            avail_hash = hashed_url[0]
+        else :
+            avail_len = len(found) + 1
+            if avail_len <= max_length:
+                avail_hash = hashed_url[0:avail_len]
+            else:
+                #increase max length and rehash
+                max_length += 1
+                hashed_url = hashlib.sha224(hashed_url).hexdigest()
+                # this will not find the shortest possbile hash - 
+                # there could be a high number of collisions for reasons of cioncidence or malevolence
+                # if production shows longer-than-expected urls being generated, changes could be made - 
+                # this could be changed to generate random stings, use a dift hash, just increment in base 60, etc
+
+    
+    new_entry = Url(short_url=avail_hash, source_url=source_url)
+    new_entry.save()
+    return new_entry
+
+
+
